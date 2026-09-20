58ad387b978f1194226873508e39ebcaa0cdbf42
Saravanan J <saravanan12220@gmail.com>
2026-09-20 21:24:19 +0530

    Add frontend application routing

---
+++ papertrail-frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import LandingPage from './LandingPage.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/app" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
